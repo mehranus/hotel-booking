7ee9efc8fa278d916a870aeaeb40605538277d51
mehran feriduni <m3hr4nus@gmail.com>
2026-02-11 10:02:28 +0330

    add module redisLock

--- src/modules/bookings/bookings.module.ts
+++ src/modules/bookings/bookings.module.ts
@@ -4,9 +4,10 @@ import { BookingsController } from './bookings.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookingEntity } from './entity/booking.entity';
 import { RoomsEntity } from '../rooms/entity/rooms.entity';
+import { RedisModule } from 'src/common/redis/redis.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([BookingEntity,RoomsEntity])],
+  imports:[TypeOrmModule.forFeature([BookingEntity,RoomsEntity]),RedisModule],
   providers: [BookingsService],
   controllers: [BookingsController]
 })
--- src/modules/bookings/bookings.service.ts
+++ src/modules/bookings/bookings.service.ts
@@ -1,21 +1,43 @@
-import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BookingEntity } from './entity/booking.entity';
 import { DataSource, Repository } from 'typeorm';
 import { RoomsEntity } from '../rooms/entity/rooms.entity';
 import { CreateBookingDto } from './dto/booking.dto';
 import { BookingType } from 'src/common/enums/booking.enum';
+import Redis from 'ioredis';
 
 @Injectable()
 export class BookingsService {
   constructor(
     @InjectRepository(BookingEntity) private readonly bookingRepository:Repository<BookingEntity>,
     @InjectRepository(RoomsEntity) private readonly roomRepository:Repository<RoomsEntity>,
-    private dataSourec:DataSource
+    private dataSourec:DataSource,
+    @Inject('REDIS_CLIENT') private redis:Redis
 ){}
 
+
+ async acquireLock(key: string, ttl = 5): Promise<boolean> {
+  const result = await this.redis.set(key, 'locked', 'EX', ttl, 'NX');
+  return result === 'OK';
+}
+
+async releseLock(key:string){
+  await this.redis.del(key)
+}
+
+
+
   async create(userId:number,dto:CreateBookingDto){
-   return this.dataSourec.transaction(
+    const lockKey=`lock:room${dto.roomId}:${dto.checkIn}:${dto.checkOut}`
+    const lockAcquired =await this.acquireLock(lockKey,5)
+    if(!lockAcquired){
+      throw new ConflictException('Another booking is in progress')
+    }
+    try{
+
+    
+   return await this.dataSourec.transaction(
       'SERIALIZABLE',
       async (manger)=>{
         if(dto.checkIn >= dto.checkOut){
@@ -40,7 +62,10 @@ export class BookingsService {
 
         return manger.save(booking)
           }
-    )
+          
+    )} finally{
+      await this.releseLock(lockKey)
+    }
   
   
 
